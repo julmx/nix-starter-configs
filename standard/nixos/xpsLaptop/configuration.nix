9da977ec13a99c69878d3c206a4098c45c19177d
JME <jme@example.com>
2024-07-22 03:30:53 +0200

    typo

--- standard/nixos/xpsLaptop/configuration.nix
+++ standard/nixos/xpsLaptop/configuration.nix
@@ -102,7 +102,7 @@
         # Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["wheel", "networkmanager", "video", "audio", "input", "tss"];
+      extraGroups = ["wheel" "networkmanager" "video" "audio" "input" "tss"];
     };
   };
 
